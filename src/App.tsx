b17be8c1aefc52fe87cdfdc765117b91b655762b
Yurchuk Vasyl <mira07081984@gmail.com>
2024-04-23 09:39:28 +0300

    feat: :sparkles: add structure to App.tsx file
    add structure to App.tsx file as components and router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Works,
+  Experience,
+  Feedbacks,
+  Contact,
+  StarsCanvas,
+} from './components';
+
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
