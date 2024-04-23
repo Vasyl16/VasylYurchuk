9e25b9c17536b4177ed358567b118f22c64974bd
Yurchuk Vasyl <mira07081984@gmail.com>
2024-04-23 19:52:13 +0300

    feat(Hero.ts): :sparkles: add Hero base page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { ComputersCanvas } from '.';
 
-const Hero = () => {
+const Hero: React.FC = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full z-50 h-screen mx-auto">
+      <div
+        className={`${styles.padding} absolute max-w-7xl inset-0 top-[120px] mx-auto flex flex-row items-start gap-5 `}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi , I'm <span className="text-[#915eff]">Vasyl</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            My descriptiion <br className="sm:block hidden" />
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
