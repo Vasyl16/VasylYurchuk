832f8ce88a0c2abc20ba185361626f63395ee97e
Yurchuk Vasyl <mira07081984@gmail.com>
2024-04-24 19:48:56 +0300

    feat: :sparkles: make computer canvas adaptive , and add scroll button to it

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -5,7 +5,11 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
-const Computers: React.FC = () => {
+type ComputersProps = {
+  isMobile: boolean;
+};
+
+const Computers: React.FC<ComputersProps> = ({ isMobile }) => {
   const { scene } = useGLTF('./desktop_pc/scene.gltf');
 
   return (
@@ -13,9 +17,9 @@ const Computers: React.FC = () => {
       <hemisphereLight intensity={5} groundColor="black" />
       <pointLight intensity={2} position={[0, 0, 0]} />
       <primitive
-        scale={0.7}
+        scale={isMobile ? 0.6 : 0.7}
         object={scene}
-        position={[-0, -3.0, -1.5]}
+        position={isMobile ? [-0, -3.0, -1.25] : [-0, -3.0, -1.5]}
         rotation={[-0.01, -0.2, -0.05]}
       />
     </mesh>
@@ -23,6 +27,24 @@ const Computers: React.FC = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setisMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setisMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (e: MediaQueryListEvent) => {
+      setisMobile(e.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <>
       <Canvas
@@ -37,7 +59,7 @@ const ComputersCanvas = () => {
             maxPolarAngle={Math.PI / 2}
             minPolarAngle={Math.PI / 2}
           />
-          <Computers />
+          <Computers isMobile={isMobile} />
         </Suspense>
         <Preload all />
       </Canvas>
